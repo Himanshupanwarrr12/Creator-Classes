2ec240d31ec794fdd23df938ad85a219a4adc7e5
Himanshu Panwar <hpanwar875@gmail.com>
2025-07-12 21:16:16 +0530

    lol

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,11 +1,11 @@
-import NavBar from "./ui/NavBar.jsx";
+import Navbar from "./ui/NavBar.jsx";
 import Footer from "./ui/Footer.jsx";
 import { Outlet } from "react-router-dom";
 
 function Layout(){
   return (
     <div className="flex flex-col min-h-screen bg-gradient-to-b from-gray-900 to-navy-900 text-white">
-      <NavBar />
+      <Navbar />
       <main className="flex-grow">
         <Outlet />
       </main>
